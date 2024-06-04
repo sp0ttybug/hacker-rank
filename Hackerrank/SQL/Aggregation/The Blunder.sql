01fb2c3b719afb43d66e37ff8a3e8711659c42c3
Orcus-IRL <vkarthick986@gmail.com>
2024-06-04 07:52:27 +0530

    back to normal

--- Hackerrank/SQL/Aggregation/The Blunder.sql
+++
@@ -1,5 +0,0 @@
-Select round(avg(salary))-round(avg(replace(salary, '0', ''))) from Employees;
-
--- OR
-
-select ceil(avg(salary) - avg(to_number(replace(to_char(salary),'0')))) from employees;
--- Hackerrank/SQL/Basic-Join/asian_population.sql
+++
@@ -1,5 +0,0 @@
-SELECT SUM(City.population)
-FROM Country
-INNER JOIN City
-    ON Country.Code = City.CountryCode
-WHERE Country.Continent='Asia';
